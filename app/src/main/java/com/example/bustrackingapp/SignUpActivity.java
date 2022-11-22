60443481433969c1b403bd783f6f94a653547776
uwase-diane <diane.uwase13@gmail.com>
2022-11-22 13:41:09 +0200

    SignUp Link and login and signUp buttons

--- app/src/main/java/com/example/bustrackingapp/SignUpActivity.java
+++ app/src/main/java/com/example/bustrackingapp/SignUpActivity.java
@@ -1,14 +1,32 @@
 package com.example.bustrackingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signUpBtn =  findViewById(R.id.signUp);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpActivity();
+            }
+        });
     }
+
+    public void signUpActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
