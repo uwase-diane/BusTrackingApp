3887eefd8dcdfb001364641df02aba50b2ac3ea5
estherpolie <polieesther6@gmail.com>
2022-11-29 11:45:55 +0200

    signup review

--- app/src/main/java/com/example/bustrackingapp/MainActivity.java
+++ app/src/main/java/com/example/bustrackingapp/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.bustrackingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView signUpLink;
+    private EditText email, password;
+    private Button loginBtn;
+    private FirebaseAuth fAuth;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.emailInput);
+        password = findViewById(R.id.passwordInput);
+        loginBtn = findViewById(R.id.loginbutton);
+        fAuth = FirebaseAuth.getInstance();
 
         signUpLink = findViewById(R.id.signUpLink);
 
@@ -27,7 +43,43 @@ public class MainActivity extends AppCompatActivity {
                 signUp();
             }
         });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emails = email.getText().toString().trim();
+                String npasswrd = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(emails)) {
+                    email.setError("email is required");
+                }
+                if (TextUtils.isEmpty(npasswrd)) {
+                    password.setError("password is required");
+                }
+                if (npasswrd.length() < 6) {
+                    password.setError(("password must be >= 6characters"));
+                }
+                fAuth.signInWithEmailAndPassword(emails,npasswrd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "logged in succefully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "ERROR!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+
+
+        });
     }
+
 
     public void signUp() {
         Intent intent = new Intent(this, SignUpActivity.class);
